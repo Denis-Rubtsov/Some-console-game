1c012a40c98c9897fe5f88c72f1532580cbb9aff
Денис Рубцов <dr6750455@gmail.com>
2024-01-22 22:08:07 +0500

    Added: new enemy names; misses; critical damages; attack directions

---
+++ HomeworkConsoleGame/Characters/AttackDirections.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkConsoleGame.Characters
+{
+    public enum AttackDirections
+    {
+        Head = 1,
+        Torso = 2,
+        Hands = 3,
+        Legs = 4,
+    }
+}
